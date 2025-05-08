59d80fe7dc65529ef9f033cb7a31c5c54399b9b8
AndikaMTR12 <andiseven17@gmail.com>
2025-05-08 15:11:31 +0800

    fix: update code

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: bash -c "./install.sh && python app.py"
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install detectron2 setelah torch tersedia
+pip install 'git+https://github.com/facebookresearch/detectron2.git'

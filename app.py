f422bf843842c939d0b44fd1237d421f2cd73aa6
AndikaMTR12 <andiseven17@gmail.com>
2025-05-08 14:46:07 +0800

    fix: model url dan anotasi url

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # ✅ ID Google Drive
-MODEL_URL = "https://drive.google.com/uc?id=1ABCDXYZ"  # Ganti dengan ID model_final.pth kamu
-ANNOTATION_URL = "https://drive.google.com/uc?id=1WXYZABC"  # Ganti dengan ID annotation JSON kamu
+MODEL_URL = "https://drive.google.com/file/d/1NqsaKb6WpvzbTdrZPK3lELgkpH9pm_Pg/view?usp=sharing"  # Ganti dengan ID model_final.pth kamu
+ANNOTATION_URL = "https://drive.google.com/file/d/1NVF-CMGa8FfZUYYFITUusSC8JgToOnIO/view?usp=sharing"  # Ganti dengan ID annotation JSON kamu
 
 MODEL_PATH = "model_final.pth"
 ANNOTATION_PATH = "annotations_coco_resized.json"
